acd87e0f65daa28749930b4059fa1aa5ce03ee82
OguraShota <p1_ku6@icloud.com>
2020-12-21 13:40:59 +0900

    プロフィールに物件一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @properties = @user.properties.paginate(page: params[:page], per_page: 5)
   end
 
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,11 @@ User.create!(name:  "竈門 炭治郎",
               password:               password,
               password_confirmation:  password)
   end
+
+  10.times do |n|
+    Property.create!(name: "Shimokitaマンション",
+                 description: "下北沢駅南口より徒歩5分の物件です！",
+                 reference: "https://suumo.jp/library/tf_13/sc_13112/to_1000617572/",
+                 recommend: 5,
+                 user_id: 1)
+  end

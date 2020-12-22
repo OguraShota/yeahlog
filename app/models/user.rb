58329e48f844941706c31997c9c6aac04fb3cff3
OguraShota <p1_ku6@icloud.com>
2020-12-22 19:39:40 +0900

    モデルのテスト

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   has_many :properties, dependent: :destroy
-  has_many :relationship,   class_name: "Relationship",
-                            foreign_key: "follower_id",
-                            dependent: :destroy
+  has_many :active_relationships,   class_name: "Relationship",
+                                    foreign_key: "follower_id",
+                                    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :passive_relationships,   class_name: "Relationship",
+                                    foreign_key: "followed_id",
+                                    dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   attr_accessor :remember_token
   before_save :downcase_email
@@ -48,6 +53,26 @@ class User < ApplicationRecord
    # フィード一覧を取得
    def feed
     Property.where("user_id = ?", id)
+  end
+
+  # ユーザーをフォローする
+  def follow(other_user)
+    following << other_user
+  end
+
+  # ユーザーをフォロー解除する
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  # 現在のユーザーがフォローしていたらtrueを返す
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+  # 現在のユーザーがフォローされていたらtrueを返す
+  def followed_by?(other_user)
+    followers.include?(other_user)
   end
     
   private
